39875d5f3540993fc6df3daf9f014b4fe3ab3a0a
Laura <lcamac02@calpoly.edu>
2020-04-05 23:56:24 -0700

    added planets code

--- planets.py
+++ planets.py
@@ -1,7 +1,9 @@
 def weight_on_planets():
    # write your code here
+   weight = float(input("What is you weight in pounds? "))
    
-   
-   
+   print("\nOn Mars you would weigh", weight*0.38,
+ "pounds.\n""On Jupiter you would weigh", weight*2.34, "pounds.") 
+
 if __name__ == '__main__':
    weight_on_planets()
